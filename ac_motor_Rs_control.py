f77f7e498e1f19b4ef5d6222ca07b620b1de0361
Ing. Di Filippo <davidedifilippo@gmail.com>
2024-05-11 06:48:59 +0200

    Create ac_motor_Rs_control.py
    Sistemare la variazione di Rs tramite un ciclo

---
+++ ac_motor_Rs_control.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Tensione, frequenza di alimentazione
+
+f = 50
+V_s_linea = 230
+
+# Parametri della macchina (collegamento a stella)
+
+p = 2
+Rs = 0.295
+Rr = 0.379
+Xm = 22.243
+Xls = Xlr = 0.676
+
+# scorrimento a carico
+
+
+f_sinc = f/2
+n_sinc = (f_sinc * 60)
+
+print("------------------")
+print("All'avviamento s = 1 -> rotore fermo")
+
+s = np.linspace(0.001, 1)
+
+# impedenza rotorica
+
+Zr = Rr/s + Xlr*1j
+
+
+Zin = Rs + Xls*1j + (Zr * Xm*1j)/(Zr + Xm*1j)
+
+
+Zin_x = np.real(Zin)
+Zin_y = np.imag(Zin)
+
+# Coordinate polari (M, phi)
+
+Zin_M = abs(Zin)
+Zin_phi = np.angle(Zin)
+FP = np.cos(Zin_phi)
+
+
+V_s = V_s_linea/np.sqrt(3)
+I_s = V_s / Zin
+I_s_eff = abs(I_s)
+I_r = I_s * (Xm*1j)/(Zr+Xm*1j)
+I_r_eff = abs(I_r)
+Coppia_mecc = (1 / (2*np.pi*f_sinc)) * (I_r_eff * I_r_eff) * (Rr / s)
+
+plt.figure(1)
+plt.plot(f_sinc * 60 - s * f_sinc * 60, Coppia_mecc,  label='Rr = 0.379')
+
+plt.figure(2)
+plt.plot(f_sinc * 60 - s * f_sinc * 60, I_s_eff, label='Rr = 0.379')
+
+Rr = 0.5
+
+# impedenza rotorica
+
+Zr = Rr/s + Xlr*1j
+
+
+Zin = Rs + Xls*1j + (Zr * Xm*1j)/(Zr + Xm*1j)
+
+
+Zin_x = np.real(Zin)
+Zin_y = np.imag(Zin)
+
+# Coordinate polari (M, phi)
+
+Zin_M = abs(Zin)
+Zin_phi = np.angle(Zin)
+FP = np.cos(Zin_phi)
+
+
+V_s = V_s_linea/np.sqrt(3)
+I_s = V_s / Zin
+I_s_eff = abs(I_s)
+I_r = I_s * (Xm*1j)/(Zr+Xm*1j)
+I_r_eff = abs(I_r)
+Coppia_mecc = (1 / (2*np.pi*f_sinc)) * (I_r_eff * I_r_eff) * (Rr / s)
+
+# disegno le risposte al variare delle velocità
+plt.figure(1)
+plt.plot(f_sinc * 60 - s * f_sinc * 60, Coppia_mecc, label='Rr = 0.5')
+
+
+plt.figure(2)
+plt.plot(f_sinc * 60 - s * f_sinc * 60, I_s_eff, label='Rr = 0.5')
+
+Rr = 0.8
+
+# impedenza rotorica
+
+Zr = Rr/s + Xlr*1j
+
+
+Zin = Rs + Xls*1j + (Zr * Xm*1j)/(Zr + Xm*1j)
+
+
+Zin_x = np.real(Zin)
+Zin_y = np.imag(Zin)
+
+# Coordinate polari (M, phi)
+
+Zin_M = abs(Zin)
+Zin_phi = np.angle(Zin)
+FP = np.cos(Zin_phi)
+
+
+V_s = V_s_linea/np.sqrt(3)
+I_s = V_s / Zin
+I_s_eff = abs(I_s)
+I_r = I_s * (Xm*1j)/(Zr+Xm*1j)
+I_r_eff = abs(I_r)
+Coppia_mecc = (1 / (2*np.pi*f_sinc)) * (I_r_eff * I_r_eff) * (Rr / s)
+
+# disegno le risposte al variare delle velocità
+plt.figure(1)
+plt.plot(f_sinc * 60 - s * f_sinc * 60, Coppia_mecc,  label='Rr = 0.8')
+
+
+plt.ylabel('Coppia motrice')
+plt.xlabel('rpm')
+plt.title("Andamento della coppia motrice")
+plt.legend()
+plt.grid()
+
+
+plt.figure(2)
+
+plt.plot(f_sinc * 60 - s * f_sinc * 60, I_s_eff, label='Rr = 0.8')
+plt.legend()
+plt.ylabel('Corrente assorbita')
+plt.xlabel('rpm')
+plt.title("Andamento della corrente")
+plt.grid()
+plt.show()
+
+
+
+
